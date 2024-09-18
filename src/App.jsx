26754de5f00f405461b3b77f5b8a9136488ffa0f
Cplm <cristianpalacios193@gmail.com>
2024-09-17 20:08:27 -0500

    my first components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { MyFirstComponets } from './components/MyFirstComponents'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Fundamentos de React </h1>
+      <MyFirstComponets />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyFirstComponents.jsx
@@ -0,0 +1,16 @@
+
+export const MyFirstComponets = () => {
+  return (
+    <>
+      <div>MyFirstComponets</div>
+      <h1>Temas React</h1>
+      <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados</li>
+        <li>Props</li>
+      </ul>
+    </>
+
+  );
+}
